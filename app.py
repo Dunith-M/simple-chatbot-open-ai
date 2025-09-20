e11eb2b955d75d2b24fac644f1be974c0aa4db67
Dunith Munasinghe <113714394+Dunith-M@users.noreply.github.com>
2025-09-20 17:34:56 +0530

    update

--- app.py
+++ app.py
@@ -7,12 +7,20 @@ OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
 
 client = OpenAI()
 
-response = client.chat.completions.create(
-    model = "gpt-3.5-turbo",
-    max_tokens = 50,
-    n=1,
-    temperature=0,
-    messages = [{"role": "user", "content": "Hello"}]
-)
+while True: 
+    question = input("\nUser😁: ")
 
-print(response)
+    if question == "exit":
+        break
+
+    response = client.chat.completions.create(
+        model = "gpt-3.5-turbo",
+        max_tokens = 50,    # How many maximum words to generate
+        n=1,                
+        temperature=0,      # How random the response is (0 to 1)
+        messages = [{"role": "user", "content": question}]
+    )
+
+#print(response)
+    for choice in response.choices:
+        print(f"\nAI🤖: {choice.message.content}")
